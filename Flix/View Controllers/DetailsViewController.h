64d638ffdaa1c34ed5bed77b5293cffcf410395a
Santino L Ramos <santinoramos@fb.com>
2021-06-23 22:57:46 -0400

    first commit, implemented steps up to 7.

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Santino L Ramos on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/View Controllers/MoviesViewController.m
@@ -0,0 +1,122 @@
+//
+//  MoviesViewController.m
+//  Flix
+//
+//  Created by Santino L Ramos on 6/23/21.
+//
+
+#import "MoviesViewController.h"
+#import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
+#import "DetailsViewController.h"
+
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.activityIndicator startAnimating];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Title"message:@"Message"preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+    // handle cancel response here. Doing nothing will dismiss the view.
+    }];
+    // add the cancel action to the alertController
+    [alert addAction:cancelAction];
+
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    // handle response here.
+    }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView addSubview:self.refreshControl];
+    
+   
+}
+
+- (void)fetchMovies{
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+               if([[error localizedDescription] isEqual:]@"The Internet connection appears to be offline." ){
+                   NSLog(@"ayo");
+               }
+               NSLog(@"hello");
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               self.movies = dataDictionary[@"results"];
+               for (NSDictionary *movie in self.movies){
+                   NSLog(@"%@", movie[@"title"]);
+                   
+               }
+               [self.tableView reloadData];
+           }
+        [self.refreshControl endRefreshing];
+        [self.activityIndicator stopAnimating];
+       }];
+    [task resume];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
+    
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.titleLabel.text = movie[@"title"];
+    cell.synopsisLabel.text = movie[@"overview"];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.posterView.image = nil;
+    
+    [cell.posterView setImageWithURL:posterURL];
+    
+    
+    return cell;
+}
+
+
+#pragma mark - Navigation
+
+ //In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+     //Get the new view controller using [segue destinationViewController].
+     //Pass the selected object to the new view controller.
+    
+    UITableViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.tableView indexPathForCell:tappedCell];
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    detailsViewController.movie = movie;
+}
+
+
+@end
