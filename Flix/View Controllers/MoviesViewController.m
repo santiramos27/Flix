da84d48df44bb701268f0f02d4faf9f4adcf5e93
Santino L Ramos <santinoramos@fb.com>
2021-06-25 22:36:10 -0400

    cleaned up code

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -89,29 +89,30 @@
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
 
     __weak MovieCell *weakSelf = cell;
-    [cell.posterView setImageWithURLRequest:request placeholderImage:nil
-                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
-                                        
-                                        // imageResponse will be nil if the image is cached
-                                        if (imageResponse) {
-                                            NSLog(@"Image was NOT cached, fade in image");
-                                            weakSelf.posterView.alpha = 0.0;
-                                            weakSelf.posterView.image = image;
+    [cell.posterView setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
+        // imageResponse will be nil if the image is cached
+        if (imageResponse) {
+            NSLog(@"Image was NOT cached, fade in image");
+            weakSelf.posterView.alpha = 0.0;
+            weakSelf.posterView.image = image;
                                             
-                                            //Animate UIImageView back to alpha 1 over 0.3sec
-                                            [UIView animateWithDuration:0.3 animations:^{
-                                                weakSelf.posterView.alpha = 1.0;
-                                            }];
-                                        }
-                                        else {
-                                            NSLog(@"Image was cached so just update the image");
-                                            weakSelf.posterView.image = image;
-                                        }
-                                    }
-                                    failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
-                                        // do something for the failure conditio
+            //Animate UIImageView back to alpha 1 over 0.3sec
+            [UIView animateWithDuration:0.3 animations:^{
+                weakSelf.posterView.alpha = 1.0;
+                
+            }];
+            
+        }
+        else {
+            NSLog(@"Image was cached so just update the image");
+            weakSelf.posterView.image = image;
+            
+        }
+        
+    }
+        failure:^(NSURLRequest *request, NSHTTPURLResponse * response, NSError *error) {
+        // do something for the failure condition
     }];
-    
     
     return cell;
 }
